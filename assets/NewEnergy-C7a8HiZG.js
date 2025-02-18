5cd5da9a1f821a65b7dcddeb6e62ab9fa65ab41a
jackchen <jackchen@example.com>
2025-02-18 14:29:05 +0800

    1

---
+++ assets/NewEnergy-C7a8HiZG.js
@@ -0,0 +1,1 @@
+import{L as a,a as r}from"./list-item-3-v1RdR660.js";import{c as s,w as o,o as i,a as n}from"./index-BemLVhBv.js";import"./IconBack-BjDbu16y.js";const x={__name:"NewEnergy",setup(c){const e=[{src:r,text:"光储一体化建设投资",detailPageUrl:"/new-energy-1"}];return(p,t)=>(i(),s(a,{ListData:e},{title:o(()=>t[0]||(t[0]=[n(" 能源管理 ")])),_:1}))}};export{x as default};
